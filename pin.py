b7c9261bdadb34aa8c2fed6920008da2ceee4b75
Nuliflyer <tyler@hyperweb.ca>
2017-11-21 15:50:55 -0500

    Added Set / Get functions

--- pin.py
+++ pin.py
@@ -12,9 +12,34 @@ class pin:
         self.a = angles[0]
         self.b = angles[1]
 
+    def getHeight(self):
+        return self.h
+
+    def getRadius(self):
+        return self.r
+
+    def getAngles(self):
+        return [self.a, self.b]
+
+    def getPosition(self):
+        return [self.x, self.y, self.z]
+
+    def getMass(self):
+        return self.m
+
+    def setPosition(self, point):
+        self.x, self.y, self.z = point
+
+    def setAngle(self, angles):
+        self.a, self.b = angles
+    #currently, this is a cylinder.
     def collision(self, point):
-        if (((self.x - point[0])**2 + (self.y - point[1])**2 + (self.z - point[2])**2)**0.5 < self.h + self.r):
+        if (((self.x - point[0])**2 + (self.y - point[1])**2 + (self.z - point[2])**2)**0.5 < self.h/2 + self.r):
+            print ""
             #Inside potential radius
+
+
 
 bowlingPin = pin([0,5,0], 5, 10, 1, [0, 0])
 bowlingPin.collision([0,5,0])
+bowlingPin.setPosition([0, 10, 0])
