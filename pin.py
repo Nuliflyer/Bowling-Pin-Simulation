f8d6882647dc6498f821eb38f30cd50d7c4e220a
Nuliflyer <tyler@hyperweb.ca>
2017-11-26 22:30:23 -0500

    Working on the elipsoid version of the collision, didn't want to overwrite master.

--- pin.py
+++ pin.py
@@ -1,26 +1,19 @@
 class pin:
-    def __init__(self, position, velocity, mass, height, radius, angles):
+    def __init__(self, position, velocity, mass, angles):
         #x y z refer to the center of mass.
         #According to bowling.com the pin is made of a solid material coated by
         #another material, so we assume the mass is uniformly distributed
+
         self.x = point[0]
         self.y = point[1]
         self.z = point[2]
         self.m = mass
-        self.h = height
-        self.r = radius
         self.a = angles[0]
         self.b = angles[1]
+        self.definition = myfunc
 
+    def insideObject(x, y, z):
 
-    def getHeight(self):
-        return self.h
-
-    def getRadius(self):
-        return self.r
-
-    def getAngles(self):
-        return [self.a, self.b]
 
     def getPosition(self):
         return [self.x, self.y, self.z]
@@ -37,10 +30,14 @@ class pin:
     def setVelocity(self, velocity):
         self.vx, self.vy, self.vz = velocity
 
+    def getAngles(self):
+        return [self.a, self.b]
+
     def setAngle(self, angles):
         self.a, self.b = angles
-    #currently, this is a cylinder.
 
+    def getDefinition(self):
+        return self.definition
 
 bowlingPin = pin([0,5,0], 5, 10, 1, [0, 0])
 bowlingPin.collision([0,5,0])
