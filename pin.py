391b83258de4eb39ff0f416b40c73d383a5b2dae
Nuliflyer <tyler@hyperweb.ca>
2017-11-27 14:17:38 -0500

    Making code work now

--- pin.py
+++ pin.py
@@ -1,29 +1,19 @@
+
+
 class pin:
-    def __init__(self, position, velocity, mass, height, radius, angles):
-        #x y z refer to the center of mass.
-        #According to bowling.com the pin is made of a solid material coated by
-        #another material, so we assume the mass is uniformly distributed
-        self.x = point[0]
-        self.y = point[1]
-        self.z = point[2]
-        self.vx = velocity[0]
-        self.vy = velocity[1]
-        self.vz = velocity[2]
+    #Position is origin of pin, angles are xy and yz angles, definition is stretch factors on elipsoid. (a, b, c)
+    def __init__(self, position, velocity, mass, angles, definition):
+        self.definition = definition
+        self.x = position[0]
+        self.y = position[1]
+        self.z = position[2]
         self.m = mass
-        self.h = height
-        self.r = radius
         self.a = angles[0]
         self.b = angles[1]
+        self.definition = definition
 
-
-    def getHeight(self):
-        return self.h
-
-    def getRadius(self):
-        return self.r
-
-    def getAngles(self):
-        return [self.a, self.b]
+    def getDefinition(self):
+        return self.definition
 
     def getPosition(self):
         return [self.x, self.y, self.z]
@@ -40,11 +30,16 @@ class pin:
     def setVelocity(self, velocity):
         self.vx, self.vy, self.vz = velocity
 
+    def getAngles(self):
+        return [self.a, self.b]
+
     def setAngle(self, angles):
         self.a, self.b = angles
-    #currently, this is a cylinder.
+
+    def getDefinition(self):
+        return self.definition
 
 
-# bowlingPin = pin([0,5,0], 5, 10, 1, [0, 0])
-# bowlingPin.collision([0,5,0])
-# bowlingPin.setPosition([0, 10, 0])
+bowlingPin = pin([0,5,0], 5, 10, [0, 0], [1, 1, 1])
+collision([0,5,0])
+bowlingPin.setPosition([0, 10, 0])
