7b1bb4e7ff404d2788b4860b827f8dfe8b77a4e8
Nuliflyer <tyler@hyperweb.ca>
2017-11-27 13:00:09 -0500

    Hmm this one didn't push odd

--- collision.py
+++ collision.py
@@ -4,5 +4,6 @@ def collision(pin, ball):
     diff = np.subtract(pin.getPosition(), ball.getPosition())
     definition = pin.getDefinition()
     if (diff[0]**2 / definition[0]**2) + (diff[1]**2 / definition[1]**2) + (diff[2]**2 / definition[2]**2) > 1:
+        return false
     else:
         return true
